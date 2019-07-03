573bfe5577f9d637bd9a6108603e278ec879cc6a
Rahul <dholariya@gmail.com>
2019-07-03 02:32:41 -0400

    Micro Host Django v1.0

---
+++ Hello/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, Docobhai , Saru ne.")
+
+# Create your views here.
