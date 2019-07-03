535015088cc4fb346d081eb5c94ed673f9c6c19a
test-git <dholariyarahul@gmail.com>
2019-07-03 12:16:50 +0530

    H1 tag added

--- Hello/views.py
+++ Hello/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Docobhai , Saru ne.")
+    return HttpResponse("Hello, Docobhai , Saru ne. <h1> MicroHost  sucsessfull.</h1>")
 
 # Create your views here.
